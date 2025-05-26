fa6fdd0030143e723caec03e76b688758a2e7cf0
unknown <khadkasalina868@gmail.com>
2025-05-26 18:23:15 -0500

    Adding some basic files

---
+++ 02counter/src/App.jsx
@@ -0,0 +1,37 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  let [counter, setCounter] = useState(1)
+
+  //let counter = 5
+
+  const addValue=()=>
+  {
+    // if (counter < 20)
+      setCounter(counter+1)
+  }
+
+  const removeValue=()=>
+  {
+    // if (counter < 0)
+    setCounter(counter-1)
+  }
+
+  return (
+    <>
+      <h1> chai aur react</h1>
+      <h2>Counter value: {counter > 20 || counter < 0 ? "Invalid Value" : counter}</h2>
+      <button
+      onClick={addValue}>Add value</button>
+      <br />
+      <button
+      onClick={removeValue}>Remove value</button>
+    </>
+  )
+}
+
+export default App
